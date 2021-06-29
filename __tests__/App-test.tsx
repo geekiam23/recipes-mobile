f8d8759a15c646211da439852468fe57835f5341
Will Larry <will23larry@gmail.com>
2021-06-29 14:17:26 -0400

    Setup jest and react-test-library

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,12 +1,8 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import LoginScreen from '../src/containers/LoginScreen';
-import {render, screen} from '@testing-library/react';
+import App from '../App';
+import {render} from '@testing-library/react-native';
 
 it('renders correctly', () => {
-  render(<LoginScreen />);
+  render(<App />);
 });
